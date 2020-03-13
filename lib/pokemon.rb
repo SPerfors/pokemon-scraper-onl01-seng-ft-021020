4e5eb043b75e5398b2d70b1f58f672810b9df7e7
Steven Perfors <stevecanbuild@gmail.com>
2020-03-13 16:46:25 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db 
+  
+  def initialize(keywords)
+    
+  end
+  
+  def save 
+    sql = <<-SQL 
+      INSERT INTO students (name, type)
+      VALUES (?,?)
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.type)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+  
 end
